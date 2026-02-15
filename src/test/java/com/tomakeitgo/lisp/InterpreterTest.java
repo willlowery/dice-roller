b3519930d2fff8436482b9bef3691fb87c0bdbe5
willlowery <will.lowery@tomakeitgo.com>
2026-02-14 20:55:59 -0700

    consolidate tests on InterpreterTest

--- src/test/java/com/tomakeitgo/lisp/InterpreterTest.java
+++ src/test/java/com/tomakeitgo/lisp/InterpreterTest.java
@@ -1,10 +1,12 @@
 package com.tomakeitgo.lisp;
 
 import com.tomakeitgo.lisp.SExpression.SAtom;
+import com.tomakeitgo.lisp.SExpression.SList;
+import com.tomakeitgo.lisp.SExpression.SNumber;
+import com.tomakeitgo.lisp.SExpression.SText;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.math.BigDecimal;
@@ -12,99 +14,98 @@ import java.util.List;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.params.provider.Arguments.of;
 
 class InterpreterTest {
 
     @ParameterizedTest
-    @CsvSource(
-            quoteCharacter = '|',
-            value = {
-                    "42,        NUMBER,  42",
-                    "'hello',   TEXT,    hello",
-                    "(),        LIST,    ",
-                    "foo,       ATOM,    foo",
-            })
-    void selfEvaluating(String input, String type, String value) {
-        var result = eval(input);
-        switch (type) {
-            case "NUMBER" -> assertEquals(new SExpression.SNumber(new BigDecimal(value)), result);
-            case "TEXT" -> assertEquals(new SExpression.SText(value), result);
-            case "LIST" -> assertEquals(new SExpression.SList(java.util.List.of()), result);
-            case "ATOM" -> assertEquals(new SAtom(value), result);
-        }
+    @MethodSource("testInterpreterInputs")
+    void testInterpreter(SExpression expected, String input) {
+        assertEquals(expected, eval(input));
     }
 
-    @ParameterizedTest
-    @CsvSource({
-            "(if true 1 2),              1",
-            "(if false 1 2),             2",
-            "(if (isEqual 1 1) 1 2),     1",
-            "(if (isEqual 1 2) 1 2),     2",
-    })
-    void ifBranches(String input, String expected) {
-        assertEquals(new SExpression.SNumber(new BigDecimal(expected)), eval(input));
+    static Stream<Arguments> testInterpreterInputs() {
+        return Stream.of(
+                // self evaluations
+                of(new SNumber(new BigDecimal("42")), "42"),
+                of(new SText("hello"), "'hello'"),
+                of(new SList(List.of()), "()"),
+                of(new SAtom("foo"), "foo"),
+
+                // if
+                of(new SNumber(new BigDecimal("1")), "(if true 1 2)"),
+                of(new SNumber(new BigDecimal("2")), "(if false 1 2)"),
+                of(new SNumber(new BigDecimal("1")), "(if (isEqual 1 1) 1 2)"),
+                of(new SNumber(new BigDecimal("2")), "(if (isEqual 1 2) 1 2)"),
+
+                // isEqual
+                of(new SAtom("true"), "(isEqual 1 1)"),
+                of(new SAtom("false"), "(isEqual 1 2)"),
+                of(new SAtom("false"), "(isEqual 1 2)"),
+                of(new SExpression.Error("isEqual requires at least two arguments"), "(isEqual 1)"),
+
+                // type checks
+                of(new SAtom("true"), "(type/isNumber 42)"),
+                of(new SAtom("true"), "(type/isText 'hi')"),
+                of(new SAtom("true"), "(type/isAtom foo)"),
+                of(new SAtom("true"), "(type/isList ())"),
+                of(new SAtom("false"), "(type/isNumber 'hi')"),
+
+                // number/operators
+                of(new SNumber(new BigDecimal("5")), "(number/add 2 3)"),
+                of(new SNumber(new BigDecimal("2")), "(number/sub 5 3)"),
+                of(new SNumber(new BigDecimal("12")), "(number/mul 3 4)"),
+                of(new SNumber(new BigDecimal("3")), "(number/div 12 4)"),
+                of(new SNumber(new BigDecimal("1")), "(number/mod 7 3)"),
+                of(new SNumber(new BigDecimal("3")), "(number/divInt 7 2)"),
+
+                // text/toAtom
+                of(new SAtom("text"), "(text/toAtom 'text')"),
+                of(new SExpression.Error("toAtom requires exactly one argument of type text"), "(text/toAtom )"),
+                of(new SExpression.Error("toAtom requires exactly one argument of type text"), "(text/toAtom 1)"),
+                
+                // number to text
+                of(new SText("42"), "(number/text 42)"),
+                of(new SText("3.14"), "(number/text 3.14)"),
+                of(new SText("5"), "(number/text 5.00)"),
+                of(new SExpression.Error("number/text requires exactly one argument"), "(number/text)"),
+                of(new SExpression.Error("number/text requires exactly one argument"), "(number/text 1 2)"),
+                of(new SExpression.Error("number/text requires a number argument"), "(number/text 'hello')")
+        );
     }
+    
 
     @ParameterizedTest
-    @CsvSource({
-            "(isEqual 1 1),  true",
-            "(isEqual 1 2),  false",
-    })
-    void isEqual(String input, String expected) {
-        assertEquals(new SAtom(expected), eval(input));
-    }
-
-    @ParameterizedTest
-    @CsvSource({
-            "(number/add 2 3),     5",
-            "(number/sub 5 3),     2",
-            "(number/mul 3 4),     12",
-            "(number/div 12 4),    3",
-            "(number/mod 7 3),     1",
-            "(number/divInt 7 2),  3",
-    })
-    void arithmetic(String input, String expected) {
-        assertEquals(new SExpression.SNumber(new BigDecimal(expected)), eval(input));
-    }
-
-    @ParameterizedTest
-    @CsvSource({
-            "(number/add 'a' 1)",
-            "(number/add 1)",
-            "(number/mul 'a' 1)",
-            "(number/mul 1)",
-            "(number/div 'a' 1)",
-            "(number/div 1)",
-            "(number/mod 'a' 1)",
-            "(number/mod 1)",
-            "(number/divInt 'a' 1)",
-            "(number/divInt 1)",
-            "(type/isNumber 1 2)",
-            "(isEqual 1)",
-            "(if true 1)",
-    })
+    @MethodSource("errorCasesInputs")
     void errorCases(String input) {
         assertInstanceOf(SExpression.Error.class, eval(input));
     }
 
-    @ParameterizedTest
-    @CsvSource({
-            "(type/isNumber 42),    true",
-            "(type/isText 'hi'),    true",
-            "(type/isAtom foo),     true",
-            "(type/isList ()),      true",
-            "(type/isNumber 'hi'),  false",
-    })
-    void typeChecks(String input, String expected) {
-        assertEquals(new SAtom(expected), eval(input));
+    static Stream<Arguments> errorCasesInputs() {
+        return Stream.of(
+                of("(number/add 'a' 1)"),
+                of("(number/add 1)"),
+                of("(number/mul 'a' 1)"),
+                of("(number/mul 1)"),
+                of("(number/div 'a' 1)"),
+                of("(number/div 1)"),
+                of("(number/mod 'a' 1)"),
+                of("(number/mod 1)"),
+                of("(number/divInt 'a' 1)"),
+                of("(number/divInt 1)"),
+                of("(type/isNumber 1 2)"),
+                of("(isEqual 1)"),
+                of("(if true 1)")
+        );
     }
+
 
     @Test
     void defAndReference() {
         var interpreter = new Interpreter();
         var ctx = Interpreter.createSContext();
         interpreter.eval(parse("(def x 5)"), ctx);
-        assertEquals(new SExpression.SNumber(new BigDecimal("5")), interpreter.eval(parse("x"), ctx));
+        assertEquals(new SNumber(new BigDecimal("5")), interpreter.eval(parse("x"), ctx));
     }
 
     @Test
@@ -113,7 +114,7 @@ class InterpreterTest {
         var ctx = Interpreter.createSContext();
         interpreter.eval(parse("(def x 1)"), ctx);
         interpreter.eval(parse("(def x 2)"), ctx);
-        assertEquals(new SExpression.SNumber(new BigDecimal("2")), interpreter.eval(parse("x"), ctx));
+        assertEquals(new SNumber(new BigDecimal("2")), interpreter.eval(parse("x"), ctx));
     }
 
     @Test
@@ -121,7 +122,7 @@ class InterpreterTest {
         var interpreter = new Interpreter();
         var ctx = Interpreter.createSContext();
         interpreter.eval(parse("(def id (lambda (x) x))"), ctx);
-        assertEquals(new SExpression.SNumber(new BigDecimal("7")), interpreter.eval(parse("(id 7)"), ctx));
+        assertEquals(new SNumber(new BigDecimal("7")), interpreter.eval(parse("(id 7)"), ctx));
     }
 
     @Test
@@ -129,7 +130,7 @@ class InterpreterTest {
         var interpreter = new Interpreter();
         var ctx = Interpreter.createSContext();
         interpreter.eval(parse("(def add (lambda (a b) (number/add a b)))"), ctx);
-        assertEquals(new SExpression.SNumber(new BigDecimal("5")), interpreter.eval(parse("(add 2 3)"), ctx));
+        assertEquals(new SNumber(new BigDecimal("5")), interpreter.eval(parse("(add 2 3)"), ctx));
     }
 
     @Test
@@ -138,7 +139,7 @@ class InterpreterTest {
         var ctx = Interpreter.createSContext();
         interpreter.eval(parse("(def y 10)"), ctx);
         interpreter.eval(parse("(def getY (lambda () y))"), ctx);
-        assertEquals(new SExpression.SNumber(new BigDecimal("10")), interpreter.eval(parse("(getY)"), ctx));
+        assertEquals(new SNumber(new BigDecimal("10")), interpreter.eval(parse("(getY)"), ctx));
     }
 
     @Test
@@ -152,31 +153,27 @@ class InterpreterTest {
         assertEquals(Interpreter.FALSE, eval("((lambda (x) (if (isEqual x 1) true false)) 0)"));
     }
 
-    @Test
-    void isEqualTooFewArgs() {
-        assertInstanceOf(SExpression.Error.class, eval("(isEqual 1)"));
-    }
 
     @Test
     void numberAddDecimal() {
         var result = eval("(number/add 0.1 0.2)");
-        assertInstanceOf(SExpression.SNumber.class, result);
-        assertEquals(0, new BigDecimal("0.3").compareTo(((SExpression.SNumber) result).value()));
+        assertInstanceOf(SNumber.class, result);
+        assertEquals(0, new BigDecimal("0.3").compareTo(((SNumber) result).value()));
     }
 
     @Test
     void textConcat() {
-        assertEquals(new SExpression.SText("hello world"), eval("(text/concat 'hello' ' world')"));
+        assertEquals(new SText("hello world"), eval("(text/concat 'hello' ' world')"));
     }
 
     @Test
     void textConcatSkipsNonText() {
-        assertEquals(new SExpression.SText("ab"), eval("(text/concat 'a' 42 'b')"));
+        assertEquals(new SText("ab"), eval("(text/concat 'a' 42 'b')"));
     }
 
     @Test
     void nestedCalls() {
-        assertEquals(new SExpression.SNumber(new BigDecimal("6")), eval("(number/add (number/add 1 2) 3)"));
+        assertEquals(new SNumber(new BigDecimal("6")), eval("(number/add (number/add 1 2) 3)"));
     }
 
     @Test
@@ -184,32 +181,32 @@ class InterpreterTest {
         var interpreter = new Interpreter();
         var ctx = Interpreter.createSContext();
         interpreter.eval(parse("(def countdown (lambda (n) (if (isEqual n 0 ) 0 (countdown (number/sub n 1 )) )))"), ctx);
-        assertEquals(new SExpression.SNumber(new BigDecimal("0")), interpreter.eval(parse("(countdown 5)"), ctx));
+        assertEquals(new SNumber(new BigDecimal("0")), interpreter.eval(parse("(countdown 5)"), ctx));
     }
 
     @Test
     void listAppendSingle() {
         var result = eval("(list/append () 1)");
-        assertEquals(new SExpression.SList(java.util.List.of(new SExpression.SNumber(new BigDecimal("1")))), result);
+        assertEquals(new SList(java.util.List.of(new SNumber(new BigDecimal("1")))), result);
     }
 
     @Test
     void listAppendMultiple() {
         var result = eval("(list/append () 1 2 3)");
-        assertEquals(new SExpression.SList(java.util.List.of(
-                new SExpression.SNumber(new BigDecimal("1")),
-                new SExpression.SNumber(new BigDecimal("2")),
-                new SExpression.SNumber(new BigDecimal("3"))
+        assertEquals(new SList(java.util.List.of(
+                new SNumber(new BigDecimal("1")),
+                new SNumber(new BigDecimal("2")),
+                new SNumber(new BigDecimal("3"))
         )), result);
     }
 
     @Test
     void listAppendToExisting() {
         var result = eval("((lambda (xs) (list/append xs 3)) (list/append () 1 2))");
-        assertEquals(new SExpression.SList(List.of(
-                new SExpression.SNumber(new BigDecimal("1")),
-                new SExpression.SNumber(new BigDecimal("2")),
-                new SExpression.SNumber(new BigDecimal("3"))
+        assertEquals(new SList(List.of(
+                new SNumber(new BigDecimal("1")),
+                new SNumber(new BigDecimal("2")),
+                new SNumber(new BigDecimal("3"))
         )), result);
     }
 
@@ -250,7 +247,7 @@ class InterpreterTest {
 
     @Test
     void firstReturnsFirstItem() {
-        assertEquals(new SExpression.SNumber(new BigDecimal("1")), eval("(list/first (list/append () 1 2 3))"));
+        assertEquals(new SNumber(new BigDecimal("1")), eval("(list/first (list/append () 1 2 3))"));
     }
 
     @Test
@@ -276,15 +273,15 @@ class InterpreterTest {
     @Test
     void restReturnsRemainingItems() {
         var result = eval("(list/rest (list/append () 1 2 3))");
-        assertEquals(new SExpression.SList(List.of(
-                new SExpression.SNumber(new BigDecimal("2")),
-                new SExpression.SNumber(new BigDecimal("3"))
+        assertEquals(new SList(List.of(
+                new SNumber(new BigDecimal("2")),
+                new SNumber(new BigDecimal("3"))
         )), result);
     }
 
     @Test
     void restOfSingleElementList() {
-        assertEquals(new SExpression.SList(List.of()), eval("(list/rest (list/append () 1))"));
+        assertEquals(new SList(List.of()), eval("(list/rest (list/append () 1))"));
     }
 
     @Test
@@ -306,9 +303,9 @@ class InterpreterTest {
     void restTooManyArgs() {
         assertInstanceOf(SExpression.Error.class, eval("(list/rest (list/append () 1) (list/append () 2))"));
     }
-    
+
     @Test
-    void testEval(){
+    void testEval() {
         assertInstanceOf(SExpression.Error.class, eval("(eval)"));
         assertEquals(eval("1"), eval("(eval 1)"));
         assertEquals(eval("'1'"), eval("(eval '1')"));
@@ -316,22 +313,22 @@ class InterpreterTest {
 
     @Test
     void condBasicMatch() {
-        assertEquals(new SExpression.SNumber(new BigDecimal("1")), eval("(cond (true 1))"));
+        assertEquals(new SNumber(new BigDecimal("1")), eval("(cond (true 1))"));
     }
 
     @Test
     void condMultiBranch() {
-        assertEquals(new SExpression.SNumber(new BigDecimal("2")), eval("(cond (false 1) (true 2))"));
+        assertEquals(new SNumber(new BigDecimal("2")), eval("(cond (false 1) (true 2))"));
     }
 
     @Test
     void condWithExpressions() {
-        assertEquals(new SExpression.SText("b"), eval("(cond ((isEqual 1 2) 'a') ((isEqual 1 1) 'b'))"));
+        assertEquals(new SText("b"), eval("(cond ((isEqual 1 2) 'a') ((isEqual 1 1) 'b'))"));
     }
 
     @Test
     void condFirstMatchWins() {
-        assertEquals(new SExpression.SNumber(new BigDecimal("1")), eval("(cond (true 1) (true 2))"));
+        assertEquals(new SNumber(new BigDecimal("1")), eval("(cond (true 1) (true 2))"));
     }
 
     @Test
@@ -349,67 +346,24 @@ class InterpreterTest {
         assertInstanceOf(SExpression.Error.class, eval("(cond)"));
     }
 
-    @Test
-    void numberToTextInteger() {
-        assertEquals(new SExpression.SText("42"), eval("(number/text 42)"));
-    }
-
-    @Test
-    void numberToTextDecimal() {
-        assertEquals(new SExpression.SText("3.14"), eval("(number/text 3.14)"));
-    }
-
-    @Test
-    void numberToTextStripsTrailingZeros() {
-        assertEquals(new SExpression.SText("5"), eval("(number/text 5.00)"));
-    }
-
-    @Test
-    void numberToTextNotANumber() {
-        assertInstanceOf(SExpression.Error.class, eval("(number/text 'hello')"));
-    }
-
-    @Test
-    void numberToTextNoArgs() {
-        assertInstanceOf(SExpression.Error.class, eval("(number/text)"));
-    }
-
-    @Test
-    void numberToTextTooManyArgs() {
-        assertInstanceOf(SExpression.Error.class, eval("(number/text 1 2)"));
-    }
 
     @Test
     void numberToTextComposable() {
-        assertEquals(new SExpression.SText("n=7"), eval("(text/concat 'n=' (number/text 7))"));
+        assertEquals(new SText("n=7"), eval("(text/concat 'n=' (number/text 7))"));
     }
 
     @Test
     void helpReturnsText() {
         var result = eval("(help)");
-        assertInstanceOf(SExpression.SText.class, result);
-        var text = ((SExpression.SText) result).value();
+        assertInstanceOf(SText.class, result);
+        var text = ((SText) result).value();
         assertTrue(text.contains("if"));
         assertTrue(text.contains("isEqual"));
         assertTrue(text.contains("number/add"));
         assertTrue(text.contains("lambda"));
         assertTrue(text.contains("help"));
     }
-
-    @ParameterizedTest
-    @MethodSource("toAtomInputs")
-    void testToAtom(SExpression expected, String input){
-        assertEquals(expected, eval(input));
-    }
-
-    static Stream<Arguments> toAtomInputs(){
-        return Stream.of(
-                Arguments.of(new SAtom("text"), "(text/toAtom 'text')"),
-                Arguments.of(new SExpression.Error("toAtom requires exactly one argument of type text"), "(text/toAtom )"),
-                Arguments.of(new SExpression.Error("toAtom requires exactly one argument of type text"), "(text/toAtom 1)")
-        );
-    }
-
+    
     private static SExpression eval(String input) {
         return new Interpreter().eval(parse(input), Interpreter.createSContext());
     }
