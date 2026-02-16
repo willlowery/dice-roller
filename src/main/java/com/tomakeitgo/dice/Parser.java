e6fbc945d1d418ce124541972cd8bc75eda1b91b
willlowery <will.lowery@tomakeitgo.com>
2026-02-15 20:57:51 -0700

    correct bug in Dice parser that it was unable to handle multiple dice in a row.

--- src/main/java/com/tomakeitgo/dice/Parser.java
+++ src/main/java/com/tomakeitgo/dice/Parser.java
@@ -1,6 +1,7 @@
 package com.tomakeitgo.dice;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -25,70 +26,82 @@ import java.util.stream.Collectors;
 public class Parser {
 
     public DiceExpression parse(List<Lexer.Token> tokens) {
-        if (!tokens.isEmpty()) {
-            if (tokens.getFirst().getType().equals(Lexer.Token.Type.PLUS)) {
-                V v = parseValue(tokens.subList(1, tokens.size()));
-                List<DiceExpression> expression = new ArrayList<>(v.expression());
-                expression.add(parse(v.rest()));
-                return new BinaryOperator("+", Integer::sum, expression);
-            } else if (tokens.getFirst().getType().equals(Lexer.Token.Type.MINUS)) {
-                V v = parseValue(tokens.subList(1, tokens.size()));
-                List<DiceExpression> expression = new ArrayList<>(v.expression());
-                expression.add(parse(v.rest()));
-                return new BinaryOperator("-", (a, b) -> a - b, expression);
-            } else {
-                return parseValue(tokens).expression().getFirst();
-            }
+        if (tokens.isEmpty()) {
+            return (r) -> new DiceExpression.Result("", true, 0, 0, 0);
         }
-
-        return (r) -> new DiceExpression.Result("", true,0,0,0);
+        return parse(new LinkedList<>(tokens));
     }
 
-    private V parseValue(List<Lexer.Token> tokens) {
-
-        if (tokens.size() >= 3) {
-            if (!tokens.getFirst().getType().equals(Lexer.Token.Type.DIGITS)) {
-                return new V(List.of(), tokens);
-
-            } else if (tokens.get(1).getType().equals(Lexer.Token.Type.D)) {
-                var rest = tokens.subList(3, tokens.size());
-                return new V(List.of(parseDice(tokens.subList(0, 3))), rest);
-            } else {
-                var rest = tokens.subList(1, tokens.size());
-
-                var remaining = parseValue(rest);
-                var items = new ArrayList<DiceExpression>();
-                items.add(parseConstant(tokens.getFirst()));
-                items.addAll(remaining.expression());
-                return new V(items, remaining.rest());
+    private DiceExpression parse(LinkedList<Lexer.Token> tokens) {
+        if (tokens.getFirst().getType().equals(Lexer.Token.Type.PLUS)) {
+            tokens.pop();
+            List<DiceExpression> expression = new ArrayList<>();
+            DiceExpression value;
+            while ((value = parseValue(tokens)) != null) {
+                expression.add(value);
             }
-        } else if (tokens.getFirst().getType().equals(Lexer.Token.Type.DIGITS)) {
-            return new V(List.of(parseConstant(tokens.getFirst())), tokens.subList(1, tokens.size()));
+            if (!tokens.isEmpty()) {
+                expression.add(parse(tokens));
+            }
+            return new BinaryOperator("+", (a, b) -> a + b, expression);
+        } else if (tokens.getFirst().getType().equals(Lexer.Token.Type.MINUS)) {
+            tokens.pop();
+            List<DiceExpression> expression = new ArrayList<>();
+            DiceExpression value;
+            while ((value = parseValue(tokens)) != null) {
+                expression.add(value);
+            }
+            if (!tokens.isEmpty()) {
+                expression.add(parse(tokens));
+            }
+            return new BinaryOperator("-", (a, b) -> a - b, expression);
         } else {
-            return new V(List.of(), tokens);
+            return parseValue(tokens);
+        }
+
+    }
+
+    private DiceExpression parseValue(LinkedList<Lexer.Token> tokens) {
+        var die = parseDie(tokens);
+        if (die == null) {
+            return parseNumber(tokens);
+        } else {
+            return die;
         }
     }
 
-    private record V(List<DiceExpression> expression, List<Lexer.Token> rest) {
+    private DiceExpression parseNumber(LinkedList<Lexer.Token> tokens) {
+        if (tokens.isEmpty()) return null;
+        if (!tokens.getFirst().getType().equals(Lexer.Token.Type.DIGITS)) return null;
+        var number = tokens.pop();
+        return (r) -> new DiceExpression.Result(
+                number.getValue(),
+                true,
+                Integer.parseInt(number.getValue()),
+                Integer.parseInt(number.getValue()),
+                Integer.parseInt(number.getValue())
+        );
     }
 
-    public DiceExpression parseConstant(Lexer.Token token) {
-        return (r) -> {
-            int value = Integer.parseInt(token.getValue());
-            return new DiceExpression.Result(
-                    token.getValue(),
-                    true,
-                    value,
-                    value,
-                    value
-            );
-        };
-    }
+    private DiceExpression parseDie(LinkedList<Lexer.Token> tokens) {
+        if (tokens.size() < 3) return null;
 
-    public DiceExpression parseDice(List<Lexer.Token> tokens) {
+        if (!tokens.get(0).getType().equals(Lexer.Token.Type.DIGITS)) {
+            return null;
+        }
+        if (!tokens.get(1).getType().equals(Lexer.Token.Type.D)) {
+            return null;
+        }
+        if (!tokens.get(2).getType().equals(Lexer.Token.Type.DIGITS)) {
+            return null;
+        }
+        var count = tokens.pop();
+        var d = tokens.pop();
+        var sides = tokens.pop();
+
         return (r) -> {
-            var numberOfDice = Integer.parseInt(tokens.get(0).getValue());
-            var numberOfSides = Integer.parseInt(tokens.get(2).getValue());
+            var numberOfDice = Integer.parseInt(count.getValue());
+            var numberOfSides = Integer.parseInt(sides.getValue());
 
             int total = 0;
             for (int i = 0; i < numberOfDice; i++) {
@@ -121,11 +134,27 @@ public class Parser {
             var results = items.stream().map(i -> i.eval(rand)).toList();
             var description = results.stream().map(Result::description).collect(Collectors.joining(" ")).trim();
 
+            int max = 0;
+            int min = 0;
+            int value = 0;
+            if (results.size() > 1) {
+                max = results.getFirst().max();
+                min = results.getFirst().min();
+                value = results.getFirst().value();
+            }
+            for (int i = 1; i < results.size(); i++) {
+                Result result = results.get(i);
+                max = operation.apply(max, result.max());
+                min = operation.apply(min, result.min());
+                value = operation.apply(value, result.value());
+            }
+
+
             return new Result(symbol + " " + description,
                     true,
-                    results.stream().map(Result::value).reduce(0, operation),
-                    results.stream().map(Result::min).reduce(0, operation),
-                    results.stream().map(Result::max).reduce(0, operation)
+                    value,
+                    min,
+                    max
             );
         }
     }
--- src/test/java/com/tomakeitgo/dice/ParserTest.java
+++ src/test/java/com/tomakeitgo/dice/ParserTest.java
@@ -16,30 +16,34 @@ class ParserTest {
     @Test
     void testSingleValue() {
         assertEquals("1", parseAndEval("1").description());
-        assertEquals("1D4(0)", parseAndEval("1d4").description());
+        assertEquals("1D4(4)", parseAndEval("1d4").description());
     }
 
     @Test
     void testAddition() {
+        assertEquals(15, parseAndEval("+ 1D1 1D2 1D3 1D4 1D5").value());
         assertEquals("+ 1 1", parseAndEval("+ 1 1").description());
-        assertEquals("+ 1D4(0) 1", parseAndEval("+ 1D4 1").description());
-        assertEquals("+ 1 1D4(0)", parseAndEval("+ 1 1D4").description());
-        assertEquals("+ 1D4(0) 1D4(0)", parseAndEval("+ 1D4 1D4").description());
-        
+        assertEquals(3, parseAndEval("+ 1 1 1").value());
+        assertEquals("+ 1D4(4) 1", parseAndEval("+ 1D4 1").description());
+        assertEquals("+ 1 1D4(4)", parseAndEval("+ 1 1D4").description());
+        assertEquals("+ 1D4(4) 1D4(4)", parseAndEval("+ 1D4 1D4").description());
+        assertEquals(8, parseAndEval("+ 1D4 1D4").value());
+
         assertEquals("+ 1 + 2 3", parseAndEval("+ 1 + 2 3").description());
-        assertEquals("+ 1 + 1D4(0) 3", parseAndEval("+ 1 + 1d4 3").description());
+        assertEquals("+ 1 + 1D4(4) 3", parseAndEval("+ 1 + 1d4 3").description());
     }
     
     @Test
     void testSubtraction(){
-        assertEquals("- 1 1", parseAndEval("- 1 1").description());
-        assertEquals("- 1D4(0) 1", parseAndEval("- 1D4 1").description());
-        assertEquals("- 1 1D4(0)", parseAndEval("- 1 1D4").description());
-        assertEquals("- 1D4(0) 1D4(0)", parseAndEval("- 1D4 1D4").description());
+        assertEquals(-1, parseAndEval("- 1 1 1").value());
+        assertEquals("- 1 1 1", parseAndEval("- 1 1 1").description());
+        assertEquals("- 1D4(4) 1", parseAndEval("- 1D4 1").description());
+        assertEquals("- 1 1D4(4)", parseAndEval("- 1 1D4").description());
+        assertEquals("- 1D4(4) 1D4(4)", parseAndEval("- 1D4 1D4").description());
     }
 
     private static Parser.DiceExpression.Result parseAndEval(String expression) {
         var result = new Parser().parse(new Lexer().lex(expression));
-        return result.eval(max -> 0);
+        return result.eval(max -> max);
     }
 }
